1ab331ace4cd7a4cc536d9d89cff1d8d254e71bb
hubert <hubstr@gmail.com>
2020-04-30 18:35:12 +0200

    add curl training

---
+++ k8s/application_development/02_dns/Dockerfile
@@ -0,0 +1,3 @@
+FROM debian:buster-slim
+RUN apt-get update && apt-get update -y && apt-get install curl -y
+ENTRYPOINT [ "curl" ]
--- k8s/application_development/02_dns/build.sh
+++ k8s/application_development/02_dns/build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-export APP=multistaged-builds
+export APP=curl
 export VERSION=0.0.1
 export PROJECT_NAME=ps-workspace
 export CONTAINER_REGISTRY=eu.gcr.io/$PROJECT_NAME/loodse-training
 export IMAGE=$CONTAINER_REGISTRY/$APP:$VERSION
 
-# TODO inject build version and app name
-
-docker build -t $IMAGE src/java
+docker build -t $IMAGE .
 docker push $IMAGE
-#docker run -it --rm -p 8080:8080 $IMAGE
 
