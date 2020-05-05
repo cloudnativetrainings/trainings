89f5a56b01eacb6de93ea73ad05294539523ee20
hubert <hubstr@gmail.com>
2020-05-05 13:36:51 +0200

    fix versioning

--- k8s/application_development/00_multistaged_builds/build.sh
+++ k8s/application_development/00_multistaged_builds/build.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
+export APP=app
+export VERSION=0.1.2
+export PROJECT_NAME=ps-workspace
+export CONTAINER_REGISTRY=eu.gcr.io/$PROJECT_NAME/loodse-training
+export IMAGE=$CONTAINER_REGISTRY/$APP:$VERSION
+
 # build application
 #./gradlew clean build
 
 # run application
 #java -jar build/libs/app-0.0.1.jar
 
-export APP=multistaged-builds
-export VERSION=0.0.1
-export PROJECT_NAME=ps-workspace
-export CONTAINER_REGISTRY=eu.gcr.io/$PROJECT_NAME/loodse-training
-export IMAGE=$CONTAINER_REGISTRY/$APP:$VERSION
-
-# TODO inject build version and app name
-
-docker build -t $IMAGE .
+# containerize application
+docker build -t $IMAGE . --build-arg BUILD_VERSION=$VERSION
 docker push $IMAGE
-#docker run -it --rm -p 8080:8080 $IMAGE
 
---
+++ k8s/application_development/misc.md
@@ -0,0 +1,2 @@
+git config --global user.email "hubert@loodse.com"
+git config --global user.name "Hubert"
