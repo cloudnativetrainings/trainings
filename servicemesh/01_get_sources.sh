903726f58ab9d383a4ca6f56778d05e66a499d7b
hubert <hubstr@gmail.com>
2020-07-02 17:25:26 +0200

    sed

--- servicemesh/01_get_sources.sh
+++ servicemesh/01_get_sources.sh
@@ -2,48 +2,9 @@
 
 # variables
 export PROJECT_NAME=loodse-training-playground
-export REGION=europe-west3
-export ZONE=europe-west3-a
-export CLUSTER_NAME=training-loodse
-export NETWORK_NAME=$CLUSTER_NAME
-export FIREWALL_NAME=$CLUSTER_NAME
 
-# set gcloud params
-gcloud config set project $PROJECT_NAME
-gcloud config set compute/region $REGION
-gcloud config set compute/zone $ZONE
+# clone repo
+https://github.com/loodse/k8s-exercises.git
 
-# create networks
-gcloud compute networks create $NETWORK_NAME --subnet-mode=custom
-
-# create cluster
-gcloud beta container clusters create $CLUSTER_NAME \
-    --network $NETWORK_NAME  --create-subnetwork=name=$NETWORK_NAME-subnet,range=10.0.0.0/24 \
-    --services-ipv4-cidr=10.0.1.0/24 --default-max-pods-per-node=110 \
-    --zone=$ZONE \
-    --cluster-version "1.16.9-gke.6" \
-    --machine-type "n1-standard-4" --num-nodes "2" \
-    --image-type "UBUNTU" --disk-type "pd-standard" --disk-size "50" \
-    --enable-network-policy --enable-ip-alias --no-enable-autoupgrade --no-enable-stackdriver-kubernetes \
-    --addons HorizontalPodAutoscaling,HttpLoadBalancing
-    # --cluster-version "1.14.10-gke.41" \
-
-### add firewall rule for ingreess gateway
-gcloud compute firewall-rules create $FIREWALL_NAME-ingress-gateway \
---network $NETWORK_NAME \
---direction=INGRESS \
---action=ALLOW \
---source-ranges=0.0.0.0/0 \
---rules=tcp:80
-
-### add ssh access for nodes
-gcloud compute firewall-rules create $FIREWALL_NAME-ssh \
- --direction=INGRESS \
- --network=$NETWORK_NAME \
- --action=ALLOW \
-  --source-ranges=0.0.0.0/0 \
-  --rules=tcp:22
-
-# connect to cluster
-gcloud container clusters get-credentials $CLUSTER_NAME 
-echo 'source <(kubectl completion bash)' >> ~/.bashrc && source ~/.bashrc
+# fix container registry url
+find . -type f -name "*.yaml" -exec sed -i "s/loodse-training-playground/$PROJECT_NAME/g" {} +
