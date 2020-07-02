393bfc8426f85dacc9830bc08eab2b308c98e92e
hubert <hubstr@gmail.com>
2020-07-02 17:27:12 +0200

    rename sed

--- servicemesh/01_fix_repo_location.sh
+++ servicemesh/01_fix_repo_location.sh
@@ -3,8 +3,5 @@
 # variables
 export PROJECT_NAME=loodse-training-playground
 
-# clone repo
-https://github.com/loodse/k8s-exercises.git
-
 # fix container registry url
 find . -type f -name "*.yaml" -exec sed -i "s/loodse-training-playground/$PROJECT_NAME/g" {} +
