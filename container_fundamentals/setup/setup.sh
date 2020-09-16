27f5e1ba8c96edd01840775ec64ead2e5c2008f4
Hubert Stroebitzer <hubstr@gmail.com>
2020-09-16 17:05:52 +0200

    fix setup

--- container_fundamentals/setup/setup.sh
+++ container_fundamentals/setup/setup.sh
@@ -16,10 +16,11 @@ gcloud beta compute --project=$PROJECT_NAME instances create $VM_NAME \
   --service-account=1011882185659-compute@developer.gserviceaccount.com \
   --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
   --tags=http-server \
-  --image=ubuntu-2004-focal-v20200729 --image-project=ubuntu-os-cloud \
+  --image=ubuntu-2004-focal-v20200907 --image-project=ubuntu-os-cloud \
   --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=container-training \
   --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
 
 gcloud compute --project=$PROJECT_NAME firewall-rules create $FIREWALL_NAME \
   --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 \
   --source-ranges=0.0.0.0/0 --target-tags=http-server
+
