356523fbeaca48cc1a12dba35db65ebf06e65ad7
Hubert Ströbitzer <hubstr@gmail.com>
2020-11-18 18:11:32 +0100

    Update fix_repo_location.sh

--- k8s_servicemesh/00_setup_cluster/fix_repo_location.sh
+++ k8s_servicemesh/00_setup_cluster/fix_repo_location.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-set -euxo pipefail
-
 # variables
-export PROJECT_NAME=loodse-training-playground
+if [[ -z ${PROJECT_NAME} ]]
+then
+  echo "INPUT: Type PROJECT_NAME (student-XX-project):" && read PROJECT_NAME
+fi
+
+set -euxo pipefail
 
 # fix container registry url
 find ./.. -type f -name "*.yaml" -exec sed -i "s/loodse-training-playground/$PROJECT_NAME/g" {} +
