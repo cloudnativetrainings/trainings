b112c846d40e6d6e9c2f3119d96ae3ce3b46e80f
Hubert Stroebitzer <hubstr@gmail.com>
2020-12-03 11:34:24 +0100

    automate PROJECT_NAME

--- k8s_fundamentals/99_teardown/teardown_cluster.sh
+++ k8s_fundamentals/99_teardown/teardown_cluster.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# variables
-if [[ -z ${PROJECT_NAME} ]]
-then
+PROJECT_COUNT=$(gcloud projects list --format json | jq .[].name | tr -d \" | wc -l)
+if (( PROJECT_COUNT == 1)); then
+  PROJECT_NAME=$(gcloud projects list --format json | jq .[].name | tr -d \" )
+  echo "Using project $PROJECT_NAME"
+fi
+if [[ -z $PROJECT_NAME ]]; then
   echo "INPUT: Type PROJECT_NAME (student-XX-project):" && read PROJECT_NAME
 fi
 export REGION=europe-west3
--- k8s_servicemesh/00_setup_cluster/fix_repo_location.sh
+++ k8s_servicemesh/00_setup_cluster/fix_repo_location.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# variables
-if [[ -z ${PROJECT_NAME} ]]
-then
+PROJECT_COUNT=$(gcloud projects list --format json | jq .[].name | tr -d \" | wc -l)
+if (( PROJECT_COUNT == 1)); then
+  PROJECT_NAME=$(gcloud projects list --format json | jq .[].name | tr -d \" )
+  echo "Using project $PROJECT_NAME"
+fi
+if [[ -z $PROJECT_NAME ]]; then
   echo "INPUT: Type PROJECT_NAME (student-XX-project):" && read PROJECT_NAME
 fi
 
 set -euxo pipefail
 
-# fix container registry url
+# fix paths
 find ./.. -type f -name "*.yaml" -exec sed -i "s/loodse-training-playground/$PROJECT_NAME/g" {} +
 find ./.. -type f -name "build_*.sh" -exec sed -i "s/PROJECT_NAME=loodse-training-playground/PROJECT_NAME=$PROJECT_NAME/g" {} +
